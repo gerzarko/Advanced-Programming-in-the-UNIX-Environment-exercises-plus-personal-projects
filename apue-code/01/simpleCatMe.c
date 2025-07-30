8989c488888002c037032a823ad41885e1e6bc08
gerzarko <germanzarkovich@gmail.com>
2025-07-30 18:30:28 -0400

    add error handling

--- apue-code/01/simpleCatMe.c
+++ apue-code/01/simpleCatMe.c
@@ -1,5 +1,6 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 #ifndef BUFFSIZE
 #define BUFFSIZE 32768
@@ -11,9 +12,18 @@ int main(int argc, char *argv[]) {
   FILE *fileToRead;
 
   fileToRead = fopen(argv[1], "r");
+  if (fileToRead == NULL) {
+    fprintf(stderr, "couldn't read file %s", argv[1]);
+    exit(EXIT_FAILURE);
+  }
   while ((c = fgetc(fileToRead)) != EOF) {
     if (putc(c, stdout) == EOF) {
       fprintf(stderr, "unable to write %c", c);
+      exit(EXIT_FAILURE);
     }
   }
+
+  fclose(fileToRead);
+
+  return EXIT_SUCCESS;
 }
