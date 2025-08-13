1faf921c87c9bab496a915d29c3dad386bda39ae
gerzarko <germanzarkovich@gmail.com>
2025-08-12 22:41:57 -0400

    add more things
    need to remember to check always for the permission i use in open()

--- apue-code/02/MyCode/fd_exercise.c
+++ apue-code/02/MyCode/fd_exercise.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #define _POSIX_C_SOURCE 200809L // fix for compiler error of not importing stdio
 #include <fcntl.h>              // new library for io opeations
 #include <stdio.h>
@@ -8,28 +9,32 @@ int main(int argc, char *argv[]) {
   (void)argv;
 
   int fileDesc;
-  FILE *fileDesF;
-  int fileDesFNumber;
+  // FILE *fileDesF;
+  // int fileDesFNumber;
+  char *hola = "hola";
 
   // printf("stdin:%d \n stdout:%d \n stderr:%d\n", STDIN_FILENO, STDOUT_FILENO,
   //        STDERR_FILENO);
   // //
   // printf("stdin %p", (void *)stdin);
 
-  fileDesc = open("testfile.txt", O_RDONLY);
+  fileDesc = open("testfile.txt", O_RDWR);
   if (fileDesc == -1) {
     fprintf(stderr, "file couldn't be open");
   }
 
   fprintf(stdout, "fd: %d\n", fileDesc);
 
-  fileDesF = fopen("testfile.txt", "r");
-  if (fileDesF == NULL) {
-    fprintf(stderr, "file couldn't be open");
-  }
+  // fileDesF = fopen("testfile.txt", "r");
+  // if (fileDesF == NULL) {
+  //   fprintf(stderr, "file couldn't be open");
+  // }
 
-  fileDesFNumber = fileno(fileDesF);
-  fprintf(stdout, "fd read pointer: %d\n", fileDesFNumber);
+  // fileDesFNumber = fileno(fileDesF);
+  // fprintf(stdout, "fd read pointer: %d\n", fileDesFNumber);
+
+  int bWritten = write(fileDesc, hola, strlen(hola));
+  fprintf(stdout, "wrote:%d", bWritten);
 
   return EXIT_SUCCESS;
 }
